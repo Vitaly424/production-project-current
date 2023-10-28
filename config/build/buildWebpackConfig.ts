a206a0983fd25a50d5218565df99e83361f7ab23
Vitaly Baev <vitalij.baev.98@bk.ru>
2023-10-28 10:55:47 +0300

    initial

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,29 @@
+import {BuildOptions} from "./types/config";
+import webpack from "webpack";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolvers} from "./buildResolvers";
+import {buildPlugins} from "./buildPlugins";
+import {buildDevServer} from "./buildDevServer";
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const {paths, mode, isDev} = options;
+
+    return {
+        mode: mode,
+        entry: paths.entry,
+        output: {
+            filename: "[name].[contenthash].js",
+            path: paths.build,
+            clean: true
+        },
+        module: {
+            rules: buildLoaders(options),
+            // loaders - нужны для того чтобы
+            // обрабатывать то что выходит за рамки вебпак(javaScript), это png/jpeg/gif/svg/css/scss/ts и т.д.
+        },
+        resolve: buildResolvers(),
+        plugins: buildPlugins(options),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined
+    }
+}
