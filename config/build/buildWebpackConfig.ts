24dc9c76a47e28aca8ea81554607a4874c551b8d
Vitaly Baev <vitalij.baev.98@bk.ru>
2023-11-18 08:27:57 +0300

    добавил сторибук

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,29 +1,28 @@
-import {BuildOptions} from "./types/config";
-import webpack from "webpack";
-import {buildLoaders} from "./buildLoaders";
-import {buildResolvers} from "./buildResolvers";
-import {buildPlugins} from "./buildPlugins";
-import {buildDevServer} from "./buildDevServer";
+import webpack from 'webpack';
+import path from 'path';
+import { BuildOptions } from './types/config';
+import { buildPlugins } from './buildPlugins';
+import { buildLoaders } from './buildLoaders';
+import { buildResolvers } from './buildResolvers';
+import { buildDevServer } from './buildDevServer';
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const {paths, mode, isDev} = options;
+    const { paths, mode, isDev } = options;
 
     return {
-        mode: mode,
+        mode,
         entry: paths.entry,
         output: {
-            filename: "[name].[contenthash].js",
+            filename: '[name].[contenthash].js',
             path: paths.build,
-            clean: true
+            clean: true,
         },
+        plugins: buildPlugins(options),
         module: {
             rules: buildLoaders(options),
-            // loaders - нужны для того чтобы
-            // обрабатывать то что выходит за рамки вебпак(javaScript), это png/jpeg/gif/svg/css/scss/ts и т.д.
         },
-        resolve: buildResolvers(),
-        plugins: buildPlugins(options),
+        resolve: buildResolvers(options),
         devtool: isDev ? 'inline-source-map' : undefined,
-        devServer: isDev ? buildDevServer(options) : undefined
-    }
+        devServer: isDev ? buildDevServer(options) : undefined,
+    };
 }
