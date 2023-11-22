f12ef0bbd961c560f2b1e96d3e35e2d7916f2f60
Vitaly Baev <vitalij.baev.98@bk.ru>
2023-11-22 06:44:25 +0300

    добавил модальное окно

---
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -0,0 +1,26 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { LoginForm } from '../LoginForm/LoginForm';
+
+interface LoginModalProps {
+    className?: string;
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+export const LoginModal = (props: LoginModalProps) => {
+    const { className, isOpen, onClose } = props;
+    const { t } = useTranslation();
+
+    return (
+        <Modal
+            isOpen={isOpen}
+            onClose={onClose}
+            lazy
+            className={classNames('', {}, [className])}
+        >
+            <LoginForm />
+        </Modal>
+    );
+};
