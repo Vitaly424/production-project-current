e85191889dc939fa6d13024a53c10e3417c83e41
Vitaly Baev <vitalij.baev.98@bk.ru>
2023-11-22 19:56:47 +0300

    сделал авторизацию, добавил асинхронную подгрузку редюсеров, написал тесты для редюсеров

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,7 +1,8 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
 import { Modal } from 'shared/ui/Modal/Modal';
-import { LoginForm } from '../LoginForm/LoginForm';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Suspense } from 'react';
+import { Loader } from 'shared/ui/Loader/Loader';
+import { LoginFormAsync } from '../LoginForm/LoginForm.async';
 
 interface LoginModalProps {
     className?: string;
@@ -11,16 +12,17 @@ interface LoginModalProps {
 
 export const LoginModal = (props: LoginModalProps) => {
     const { className, isOpen, onClose } = props;
-    const { t } = useTranslation();
 
     return (
         <Modal
+            className={classNames('', {}, [className])}
             isOpen={isOpen}
             onClose={onClose}
             lazy
-            className={classNames('', {}, [className])}
         >
-            <LoginForm />
+            <Suspense fallback={<Loader />}>
+                <LoginFormAsync />
+            </Suspense>
         </Modal>
     );
 };
